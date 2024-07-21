3bc948683d56589ae3bc07e5279fe97125183cfb
m-rohan171 <hadi.dev@pk.see.biz>
2024-07-21 17:18:13 +0500

    mobile responsive

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const PORT = process.env.PORT || 4000;
 const corsOptions = {
   origin: [
     "http://localhost:3000",
-    "https://6696afbaf619af6aa84dcaf9--smm-fe.netlify.app",
+    "https://669cfb71650924d7424a7208--smm-fe.netlify.app",
   ], // specify your allowed origins
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
